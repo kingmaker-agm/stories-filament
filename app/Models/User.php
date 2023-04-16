c9b9b8c4ebbcfd9e5bae83586aa1458aa4d086dc
Kingmaker <b.g.pratheep.cool@gmail.com>
2023-04-16 19:30:49 +0530

    Added Filament User access in production

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -45,5 +47,10 @@ class User extends Authenticatable
     public function stories()
     {
         return $this->hasMany(Story::class);
+    }
+
+    public function canAccessFilament(): bool
+    {
+        return Str::endsWith($this->email, '@kingmaker.co.in');
     }
 }
