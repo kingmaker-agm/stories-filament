75a8a5e694629e1a4775f2a350b68d7a617d5480
Kingmaker <b.g.pratheep.cool@gmail.com>
2023-07-11 17:10:08 +0530

    Widgets & Customisation of Dashboard
    - override Filament defaults
    - Added Stats count widget

---
+++ app/Filament/Widgets/ResourceCountWidget.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Filament\Resources\RatingTagResource;
+use App\Filament\Resources\StoryResource;
+use App\Filament\Resources\StorySeriesResource;
+use App\Filament\Resources\TagResource;
+use Awcodes\Overlook\Overlook;
+
+class ResourceCountWidget extends Overlook
+{
+    protected static ?int $sort = -1;
+
+    public function getIncludes(): array
+    {
+        return [
+            StoryResource::class,
+            StorySeriesResource::class,
+            RatingTagResource::class,
+            TagResource::class,
+        ];
+    }
+}
